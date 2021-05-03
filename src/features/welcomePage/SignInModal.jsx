3779157c132e7d2865391cc6b0bd3ec67fe7f199
Derek Jin <jsw1996@uw.edu>
2021-05-03 01:25:03 -0700

    added welcome page

---
+++ src/features/welcomePage/SignInModal.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+
+export default function SignInModal() {
+    const [open, setOpen] = React.useState(false)
+
+    return (
+        <Modal
+            closeIcon
+            open={open}
+            trigger={<Button className="button" color="olive" content='Sign in' icon='sign in' size='big' />
+            }
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+        >
+            <Header icon='archive' content='Archive Old Messages' />
+            <Modal.Content>
+                <p>
+                    Your inbox is getting full, would you like us to enable automatic
+                    archiving of old messages?
+        </p>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button color='red' onClick={() => setOpen(false)}>
+                    <Icon name='remove' /> No
+        </Button>
+                <Button color='green' onClick={() => setOpen(false)}>
+                    <Icon name='checkmark' /> Yes
+        </Button>
+            </Modal.Actions>
+        </Modal>
+    )
+}
+
--- src/features/welcomePage/WelcomePage.jsx
+++ src/features/welcomePage/WelcomePage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button } from 'semantic-ui-react'
+import SignInModal from './SignInModal'
+import RegisterModal from './RegisterModal'
 import 'semantic-ui-css/semantic.min.css'
-
+import './Welcome.css'
 import {
     BrowserRouter as Router,
     Switch,
@@ -12,10 +14,12 @@ import {
 
 export default function WelcomePage() {
     return (
-        <div style={{ height: "1100px", background: "black" }}>
-            <div style={{ width: "300px", margin: "auto" }}>
-                <Button style={{ background: "red" }} content='Sign up' icon='sign in' size='big' />
-                <Button content='Sign up' icon='signup' size='big' />
+        <div id="welcomeContainer" >
+            <div id="welcomeInner">
+                {/* <Button className="button" color="olive" content='Sign in' icon='sign in' size='big' />
+                <Button className="button" color="twitter" content='Sign up' icon='signup' size='big' /> */}
+                <SignInModal />
+                <RegisterModal />
             </div>
 
         </div>
