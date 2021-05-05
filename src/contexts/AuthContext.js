36616fef89c1481a9aa08e340a730f99d956c4fa
Shengtong Jin <jinshen1@cse.msu.edu>
2021-05-05 00:56:52 -0700

    log out

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -20,6 +20,10 @@ export function AuthProvider({ children }) {
     return auth.signInWithEmailAndPassword(email, password)
   }
 
+  function logout() {
+    return auth.signOut()
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user =>{
       setCurrentUser(user)
@@ -32,6 +36,7 @@ export function AuthProvider({ children }) {
   const value ={
     currentUser,
     login,
+    logout,
     signup
   }
 
--- src/features/nav/Navbar.jsx
+++ src/features/nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState}from "react";
 // import { Menu, Container } from "semantic-ui-react";
 
 import 'semantic-ui-css/semantic.min.css'
@@ -10,6 +10,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
+import { useAuth } from "../../contexts/AuthContext"
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   '@global': {
@@ -40,13 +42,28 @@ const useStyles = makeStyles((theme) => ({
 }));
 const Navbar = () => {
   const classes = useStyles();
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+  
+  async function handleLogout() {
+    setError("")
 
+    try {
+      await logout()
+      history.push("/")
+    } catch {
+      console.log("Failed to log out")
+      setError("Failed to log out")
+    }
+  }
     return (
     <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
       <Toolbar className={classes.toolbar}>
         <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
           Team Portal
         </Typography>
+        {error && <p >{error}</p>}
         <nav>
           <Link variant="button" color="textPrimary" href="#" className={classes.link}>
             Features
@@ -58,7 +75,7 @@ const Navbar = () => {
             Support
           </Link>
         </nav>
-        <Button href="#" color="primary" variant="outlined" className={classes.link}>
+        <Button onClick = {handleLogout} href="#" color="primary" variant="outlined" className={classes.link}>
           Log Out
         </Button>
       </Toolbar>
